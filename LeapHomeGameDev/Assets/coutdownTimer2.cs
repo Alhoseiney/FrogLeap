37f73174d70de85526c5dbaec13eddad6fa0d0b4
Mmaya0 <152199667+Mmaya0@users.noreply.github.com>
2023-12-04 16:52:08 +0300

    TimerDone

---
+++ LeapHomeGameDev/Assets/coutdownTimer2.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class coutdownTimer2 : MonoBehaviour
+{
+    public TextMeshProUGUI timerText;
+    public float startTime = 60f;
+
+    private float currentTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startTime;
+        UpdateTimerText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0f)
+        {
+            currentTime = 0f;
+            // Handle end of countdown
+        }
+
+        UpdateTimerText();
+
+    }
+    void UpdateTimerText()
+    {
+        timerText.SetText(currentTime.ToString("0.00"));
+    }
+    void RestartGame()
+    {
+        // Reload the current scene
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.name);
+    }
+}
+
